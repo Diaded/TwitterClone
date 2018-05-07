970cb33d3359c1e7a2e75dce521ee50c9ddb417e
Umair Khalil <khalilumair16@gmail.com>
2018-05-07 16:06:21 -0500

    made changes

--- controller.js
+++ controller.js
@@ -1,9 +1,15 @@
 var mongoose= require('mongoose');
 var bodyParser= require('body-parser');
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 module.exports= function(app){
 
 app.get('/', function(req, res){
   res.sendFile('./index.html');
+});
+
+app.post('/signup', urlencodedParser, function(req, res){
+  console.log(req.body);
 });
 }
