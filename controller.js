a53f8dc87061c024735cc4e138bde25b3b35f6db
Umair Khalil <khalilumair16@gmail.com>
2018-05-14 13:05:48 -0500

    finsied tweeting error, working on retweeting

--- controller.js
+++ controller.js
@@ -39,31 +39,31 @@ app.post('/login', urlencodedParser, function(req, res){
 });
 
 app.post('/tweet', urlencodedParser, function(req, res){
+  console.log(req.body);
   twitter.find({username: getter.getter()}, function(err, data){
     data[0].tweets.push({str:req.body.tweet, likes: 0, rt: 0});
     console.log(data[0]);
     twitter.update({username: data[0].username}, data[0], {upsert: true}, function(){
-      twitter.find({}, function(err, data){
-        res.render('index.ejs', {data:data});
-      });
+      console.log('working');
     });
   });
+  res.json(req.body);
 });
 
 app.post('/signup', urlencodedParser, function(req, res){
   twitter.find({email: req.body.email}, function(err, data){
     if(data.length>0){
-      res.sendFile(__dirname+'/public/errSignUp.html');
+      res.render('errSignUp.ejs', {data: 'Email already taken'});
     }else{
         twitter.find({username: req.body.username}, function(err, data){
           if(data.length>0){
-            res.sendFile(__dirname+'/public/errSignUp.html');
+            res.render('errSignUp.ejs', {data: 'Username is already taken'});
           }else{
             if(req.body.password===req.body.passwordCon){
              twitter({username: req.body.username, email: req.body.email, password: req.body.password, tweets:[]}).save();
-             res.sendFile(__dirname+'/public/index.html');
+             res.sendFile(__dirname+ '/public/index.html');
             }else{
-              res.sendFile(__dirname+'/public/errSignUp.html');
+              res.render('errSignUp.ejs', {data: 'password doesnt match'});
             }
           }
         });
@@ -100,22 +100,38 @@ app.post('/searchfor', urlencodedParser, function(req, res){
   });
 });
 
-app.post('/retweet', urlencodedParser, function(req, res){
-  console.log(req.body);
-  res.json(req.body);
-});
 
 app.post('/like', urlencodedParser, function(req, res){
-  //console.log(req.body);
+  console.log(req.body);
   twitter.find({username: req.body.username}, function(err, data){
     for(var i=0; i<data[0].tweets.length; i++){
     if(data[0].tweets[i].str===req.body.str){
       data[0].tweets[i].likes++;
       }
     }
-    console.log(data[0]);
+  //  console.log(data[0]);
     twitter.update({username:req.body.username}, data[0], {upsert:true}, function(){console.log('working')});
   });
   res.json(req.body);
 });
+
+app.post('/retweet', urlencodedParser, function(req, res){
+  console.log(req.body);
+  twitter.find({username: req.body.username}, function(err, data){
+    for(var i=0; i<data[0].tweets.length; i++){
+      if(data[0].tweets[i].str===req.body.str){
+        data[0].tweets[i].rt++;
+      }
+    }
+    twitter.update({username: req.body.username}, data[0], {upsert:true}, function(){console.log('working')});
+  });
+
+  twitter.find({username: getter.getter()}, function(err, data){
+
+  });
+
+
+  res.json(req.body);
+});
+
 }
--- public/jq.js
+++ public/jq.js
@@ -1,22 +1,7 @@
 $(document).ready(function(){
 
-
-
- $(".retweet").submit(function(){
-    var username=  $(this).parent().parent().parent().parent().find('.username').html();
-    data={username: username};
-    $.ajax({
-      type:'POST',
-      url:"/retweet",
-      data:data,
-      success: function(data){
-        location.reload();
-      }
-    });
-
-  });
-
-  $(".like").submit(function(){
+$(".like").submit(function(){
+    //e.preventDefault();
     var username=  $(this).parent().parent().parent().parent().find('.username').html();
     var str= $(this).parent().parent().parent().parent().find('.str').html();
     data={username: username, str: str};
@@ -25,15 +10,57 @@ $(document).ready(function(){
       url:'/like',
       data:data,
       success: function(data){
+        setTimeout(function(){
         location.reload();
-
+      }, 500);
       }
     });
+    return false;
+  });
 
+  $('.tweetOut').submit(function(){
+      $('.reTWEET').hide();
+      username=  $(this).parent().parent().parent().parent().find('.username').html();
+      str= $(this).parent().parent().parent().parent().find('.str').html();
+      $(this).parent().parent().parent().parent().find('.reTWEET').show();
+      return false;
   });
 
 
+$('.tweeter').submit(function(){
+  var str=$(this).find('.tweetStr').val();
+  data ={tweet: str}
 
+  $.ajax({
+    type:'POST',
+    url:'/tweet',
+    data:data,
+    success: function(data){
+      setTimeout(function(){
+      location.reload();
+    }, 500);
+    }
+  });
+  return false;
+});
 
+$('.reTWEET1').click(function(){
+  username=  $(this).parent().parent().parent().parent().find('.username').html();
+  str= $(this).parent().parent().parent().parent().find('.str').html();
+  tweetSTR=$(this).parent().parent().parent().parent().find('.reTWEET2').val();
+  var data= {username:username, str:str, tweetString:tweetSTR};
+
+  $.ajax({
+    type:'POST',
+    url:'/retweet',
+    data:data,
+    success: function(data){
+    setTimeout(function(){
+      window.location.reload();
+    }, 500);
+    }
+});
+return false;
+});
 
 });
